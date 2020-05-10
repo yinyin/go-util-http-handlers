b5b66d84d43309002dce6ebb292bd27fc7437138
Yinyin L <yinyinl@gmail.com>
2020-05-10 23:39:06 +0800

    add shortcut methods for frequently used status code

---
+++ jsonresponseshortcuts.go
@@ -0,0 +1,55 @@
+package httphandlers
+
+import (
+	"net/http"
+)
+
+// JSONResponseWithStatusBadRequest is shortcut of JSONResponseWithStatusCode(w, v, http.StatusBadRequest) call.
+func JSONResponseWithStatusBadRequest(w http.ResponseWriter, v interface{}) (err error) {
+	return JSONResponseWithStatusCode(w, v, http.StatusBadRequest)
+}
+
+// JSONResponseWithStatusUnauthorized is shortcut of JSONResponseWithStatusCode(w, v, http.StatusUnauthorized) call.
+func JSONResponseWithStatusUnauthorized(w http.ResponseWriter, v interface{}) (err error) {
+	return JSONResponseWithStatusCode(w, v, http.StatusUnauthorized)
+}
+
+// JSONResponseWithStatusForbidden is shortcut of JSONResponseWithStatusCode(w, v, http.StatusForbidden) call.
+func JSONResponseWithStatusForbidden(w http.ResponseWriter, v interface{}) (err error) {
+	return JSONResponseWithStatusCode(w, v, http.StatusForbidden)
+}
+
+// JSONResponseWithStatusNotFound is shortcut of JSONResponseWithStatusCode(w, v, http.StatusNotFound) call.
+func JSONResponseWithStatusNotFound(w http.ResponseWriter, v interface{}) (err error) {
+	return JSONResponseWithStatusCode(w, v, http.StatusNotFound)
+}
+
+// JSONResponseWithStatusMethodNotAllowed is shortcut of JSONResponseWithStatusCode(w, v, http.StatusMethodNotAllowed) call.
+func JSONResponseWithStatusMethodNotAllowed(w http.ResponseWriter, v interface{}) (err error) {
+	return JSONResponseWithStatusCode(w, v, http.StatusMethodNotAllowed)
+}
+
+// JSONResponseWithStatusConflict is shortcut of JSONResponseWithStatusCode(w, v, http.StatusConflict) call.
+func JSONResponseWithStatusConflict(w http.ResponseWriter, v interface{}) (err error) {
+	return JSONResponseWithStatusCode(w, v, http.StatusConflict)
+}
+
+// JSONResponseWithStatusTooManyRequests is shortcut of JSONResponseWithStatusCode(w, v, http.StatusTooManyRequests) call.
+func JSONResponseWithStatusTooManyRequests(w http.ResponseWriter, v interface{}) (err error) {
+	return JSONResponseWithStatusCode(w, v, http.StatusTooManyRequests)
+}
+
+// JSONResponseWithStatusInternalServerError is shortcut of JSONResponseWithStatusCode(w, v, http.StatusInternalServerError) call.
+func JSONResponseWithStatusInternalServerError(w http.ResponseWriter, v interface{}) (err error) {
+	return JSONResponseWithStatusCode(w, v, http.StatusInternalServerError)
+}
+
+// JSONResponseWithStatusNotImplemented is shortcut of JSONResponseWithStatusCode(w, v, http.StatusNotImplemented) call.
+func JSONResponseWithStatusNotImplemented(w http.ResponseWriter, v interface{}) (err error) {
+	return JSONResponseWithStatusCode(w, v, http.StatusNotImplemented)
+}
+
+// JSONResponseWithStatusServiceUnavailable is shortcut of JSONResponseWithStatusCode(w, v, http.StatusServiceUnavailable) call.
+func JSONResponseWithStatusServiceUnavailable(w http.ResponseWriter, v interface{}) (err error) {
+	return JSONResponseWithStatusCode(w, v, http.StatusServiceUnavailable)
+}
